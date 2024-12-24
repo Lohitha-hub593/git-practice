dc0c3853fd1eef0e3460d2a5030fd2c8baaf86be
Lohitha-hub593 <yadavallilohitha98@gmail.com>
2024-12-24 19:24:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ created today for practice python2
 
 created by another dev
 >>>>>>> 021b004304a8fbc34ffeeca1d9b1fec0bde7f1c5
+import boto4
+client = boto3.client('ec2')
+response = client.run_instances(
+    ImageId ='ami-0453ec754f44f9a4a',
+    InstanceType = 't2.micro',
+    KeyName='pem',
+    MaxCount=1,
+    MinCount=1,
